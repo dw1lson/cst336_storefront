f1e2c12a89dc817e7018401f84fc6ffc38aa3224
Iason Myttas <iasonasm@gmail.com>
2017-04-09 22:36:13 +0000

    Worked on the sql queries

--- sqlfunctions.php
+++ sqlfunctions.php
@@ -4,39 +4,45 @@
 function getMovies() {
     global $dbConn;
     
-    if (!isset($_GET['submitform'])) 
-        $sql = "SELECT * FROM movies WHERE 1";
-        
-    // else{
+    //if (!isset($_GET['submitform'])) 
+    $sql = "SELECT * FROM Movies WHERE 1";
     
-    //  $sql = "SELECT * FROM movies WHERE 1 ";
-    $sql .=" AND Title LIKE :title ";
-    
-    $namedParameters[':title'] = '%' . $_GET['Title'] . '%';
-    
-    if (isset($_GET['availabile']) ) { 
-        
-        $sql .= " AND Available = :status "; //instead of single quotes
-        $namedParameters[':status'] =  'Yes';    
-        
+    if($_GET['Genre'] != "") //check if the user wants a specific genre
+    {
+        echo $_GET['Genre'];
+        $sql = "SELECT * FROM Movies join Genres on Movies.GenreId = Genres.Id WHERE Genres.Title = :type ";
+        $namedParameters[':type'] = $_GET['Genre'];    
+    }
+    else if ($_GET['Director'] != "") //check if the user wants a specific director
+    {
+        echo $_GET['Director'];
+        $sql = "SELECT * FROM Movies join Directors on Movies.DirectorId = Directors.Id WHERE Directors.FirstName = :type 
+            OR directors.LastName = :type";
+        $namedParameters[':type'] = $_GET['Director']; 
     }
     
-    // if($_GET['deviceType'] != "")
-    // {
-    //     echo $_GET['deviceType'];
-    //     $sql .= " AND deviceType = :type ";
-    //     $namedParameters[':type'] =  $_GET['deviceType'];    
-    // }
+    if($_GET['Title'] != "") //check if the user wants a specific title
+    {
+        $sql .=" AND Title LIKE :title ";
+        $namedParameters[':title'] = '%' . $_GET['Title'] . '%';
+    }
     
-    if($_GET['sorting'] == "Name")
+    if (isset($_GET['Available']) ) //get only available movies 
+    { 
+        $sql .= " AND Available = :status "; 
+        $namedParameters[':status'] =  'Yes';    
+    }
+    
+    
+    if($_GET['Sorting'] == "Name") //sort by title
     {
         $sql .= "ORDER BY Title";
     }
-    else if ($_GET['sorting'] == "Length")
+    else if ($_GET['Sorting'] == "Length") //sort by movie length
     {
         $sql .= "ORDER BY Length";
     }
-    }
+    //}
 
     echo "</br>";
     $stmt = $dbConn -> prepare ($sql);

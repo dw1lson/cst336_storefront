a8d1ae77da1c84956949a37f683a42593a3427ec
Iason Myttas <iasonasm@gmail.com>
2017-04-11 16:57:53 +0000

    finished with querying (not tested)

--- sqlfunctions.php
+++ sqlfunctions.php
@@ -7,18 +7,26 @@ function getMovies() {
     //if (!isset($_GET['submitform'])) 
     $sql = "SELECT * FROM Movies WHERE 1";
     
-    if($_GET['Genre'] != "") //check if the user wants a specific genre
+    if($_GET['Genre'] != "" && $_GET['Director'] == "") //check if the user wants a specific genre
     {
-        echo $_GET['Genre'];
+        //echo $_GET['Genre'];
         $sql = "SELECT * FROM Movies join Genres on Movies.GenreId = Genres.Id WHERE Genres.Title = :type ";
         $namedParameters[':type'] = $_GET['Genre'];    
     }
-    else if ($_GET['Director'] != "") //check if the user wants a specific director
+    else if ($_GET['Director'] != "" && $_GET['Genre'] == "") //check if the user wants a specific director
     {
-        echo $_GET['Director'];
+        //echo $_GET['Director'];
         $sql = "SELECT * FROM Movies join Directors on Movies.DirectorId = Directors.Id WHERE Directors.FirstName = :type 
-            OR directors.LastName = :type";
+            OR directors.LastName = :type AND ";
         $namedParameters[':type'] = $_GET['Director']; 
+    }
+    else if($_GET['Genre'] != "" && $_GET['Director'] != "")
+    {
+        // echo $_GET['Director'];
+        $sql = "SELECT * FROM Movies join Directors on Movies.DirectorId = Directors.Id join Genres on Movies.GenreId = Genres.Id WHERE Directors.FirstName = :type 
+            OR directors.LastName = :type Genres.Title = :type2 ";
+        $namedParameters[':type'] = $_GET['Director']; 
+        $namedParameters[':type2'] = $_GET['Genre']; 
     }
     
     if($_GET['Title'] != "") //check if the user wants a specific title

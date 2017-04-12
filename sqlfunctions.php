2bb84d334dd6c7f67a5a8a65a7b7ac14318d9670
Iason Myttas <iasonasm@gmail.com>
2017-04-12 07:00:00 +0000

    Finished with sql queries and connecting them to the html forms

--- sqlfunctions.php
+++ sqlfunctions.php
@@ -13,27 +13,27 @@ $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 function getMovies() {
     global $dbConn;
     
-    //if (!isset($_GET['submitform'])) 
     $sql = "SELECT * FROM Movies WHERE 1";
     
     if($_GET['Genre'] != "" && $_GET['Director'] == "") //check if the user wants a specific genre
     {
-        //echo $_GET['Genre'];
-        $sql = "SELECT * FROM Movies join Genres on Movies.GenreId = Genres.Id WHERE Genres.Title = :type ";
+        echo "GENRE";
+        $sql = "SELECT * FROM Movies join Genres on Movies.GenreId = Genres.Id WHERE Genres.Name = :type ";
         $namedParameters[':type'] = $_GET['Genre'];    
     }
     else if ($_GET['Director'] != "" && $_GET['Genre'] == "") //check if the user wants a specific director
     {
         //echo $_GET['Director'];
         $sql = "SELECT * FROM Movies join Directors on Movies.DirectorId = Directors.Id WHERE Directors.FirstName = :type 
-            OR directors.LastName = :type AND ";
+            OR Directors.LastName = :type ";
         $namedParameters[':type'] = $_GET['Director']; 
     }
     else if($_GET['Genre'] != "" && $_GET['Director'] != "")
     {
+        echo "get BOTH";
         // echo $_GET['Director'];
         $sql = "SELECT * FROM Movies join Directors on Movies.DirectorId = Directors.Id join Genres on Movies.GenreId = Genres.Id WHERE Directors.FirstName = :type 
-            OR directors.LastName = :type Genres.Title = :type2 ";
+            OR Directors.LastName = :type AND Genres.Name = :type2 ";
         $namedParameters[':type'] = $_GET['Director']; 
         $namedParameters[':type2'] = $_GET['Genre']; 
     }
@@ -47,7 +47,7 @@ function getMovies() {
     if (isset($_GET['Available']) ) //get only available movies 
     { 
         $sql .= " AND Available = :status "; 
-        $namedParameters[':status'] =  'Yes';    
+        $namedParameters[':status'] =  $_GET['Available'];    
     }
     
     
